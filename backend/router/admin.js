1335006f64cb3eb1193e46127d57a6b1d8402b25
jfmam <49827304+jfmam@users.noreply.github.com>
2020-04-14 01:06:38 +0900

    backend router

--- backend/router/admin.js
+++ backend/router/admin.js
@@ -28,11 +28,19 @@ router.post('/',async(req,res,next)=>{
             opTimeClose:req.body.opTimeClose,
             full:req.body.full
         })
+        if(req.body.Menu){
+            if(Array.isArray(req.body.menu)){
+                  const menu = await Promise.all(req.body.Menu.map((item,index) => {
+          return db.Menu.create({ food: item.food,price:item.price,soldOut:item.soldOut });
+            }))
+            await newAdmin.addMenu(menu);
+        }
+    }
         // const menuPost=await db.Menu.create({
         //     food:req.
         // })
     }catch(e){
-
+        res.send(e);
     }
 })
 module.exports = router;
