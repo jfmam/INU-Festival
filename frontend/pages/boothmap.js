b29d4715db8970b7ad60894f1c7d955449d00449
jfmam <49827304+jfmam@users.noreply.github.com>
2020-04-24 22:26:10 +0900

    boothamap marker

--- frontend/pages/boothmap.js
+++ frontend/pages/boothmap.js
@@ -1,15 +1,46 @@
 
 /*global kakao*/
-import React, { useEffect} from "react";
+import React, { useEffect, useState, useCallback, useRef} from "react";
+import styled from 'styled-components'
 
+const OriginMarker=styled.img`
+    position:absolute;
+    left:35%;
+    top:17rem
+`
+const ClickMarker=styled.img`
+    position:absolute;
+    left:32%;
+    top:15rem
+`
 
 const Boothmap=()=>{
+    const [toggle,setToggle]=useState(false)
+    const backgroundImage=useRef();
+    const markerClick=useCallback(()=>{
+        setToggle(true);
+        backgroundImage.current.style.height='35rem'
+    },[])
+      const markerUnClick=useCallback(()=>{
+        setToggle(false);
+        backgroundImage.current.style.height='38rem'
+    },[])
 
-    
     return(
         <>
-        <img src='/boothmap.jpg' style={{width:'100%',height:'40rem'}}/>
-        <img src='/shape.png' style={{position:"absolute",left:'32%',top:'17rem'}}/>
+        <img src='/boothmap.jpg' onClick={markerUnClick} ref={backgroundImage} style={{width:'100%',height:'38rem'}}/>
+        {!toggle
+        ?<OriginMarker src='/shape.png' onClick={markerClick}/>
+        :<ClickMarker src='/clickShape.png' onClick={markerUnClick}/>
+    }
+        {toggle&&
+      
+                <div>
+                    <div ><span><strong>부스이름</strong></span><span>오픈시간</span></div>
+               <div><label>만석여부</label></div>                
+               <div style={{textAlign:'center'}}><button>더 보기</button></div> 
+                </div>
+        }
         </>
     )
 }
